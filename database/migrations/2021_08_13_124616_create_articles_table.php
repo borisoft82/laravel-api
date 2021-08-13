2c5545bd15fe2797a3f8297bdc4710ca085fd237
borisoft82 <borisoft82@gmail.com>
2021-08-13 15:45:38 +0200

    creating db structure

--- database/migrations/2021_08_13_124616_create_articles_table.php
+++ database/migrations/2021_08_13_124616_create_articles_table.php
@@ -15,6 +15,11 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('category');
+            $table->string('author');
+            $table->integer('clicks');
             $table->timestamps();
         });
     }
